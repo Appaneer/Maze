4f713b6905909253dd8208d25d563b876ad93c57
Jimmy Shi <Jimmyshi360@gmail.com>
2016-01-25 11:14:07 -0500

    Initial Commit For Jimmy

--- Assets/Script/TargetManager.cs
+++ Assets/Script/TargetManager.cs
@@ -5,7 +5,7 @@ public class TargetManager : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-		if(other.tag.Equals("Player"))
+		if(other.tag.Equals("Player"))//thia ia plyer
 			GameObject.FindGameObjectWithTag("Scripts").GetComponent<LevelManager>().isFinishingLevel = true;
     }
 }
