cb0a59b828bdbcb27ca15b3507974bb804c63d2c
xavierlu <xllgms@gmail.com>
2016-01-25 12:22:40 -0500

    add coin manager

---
+++ Assets/Script/CoinManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinManager : MonoBehaviour {
+
+	/*
+	 * This script is responible for coin management
+	 */
+
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (PlayerPrefs.GetInt("Coins"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/CoinTriggerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinTriggerManager : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag.Equals ("Player")) {
+			PlayerPrefs.SetInt ("Coins", PlayerPrefs.GetInt("Coins") + 1);
+			Destroy (gameObject);
+		}
+			
+	}
+}
