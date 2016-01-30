aa5e4dbfcc56f1f2fa95ab4264c18548096117c1
xavierlu <xllgms@gmail.com>
2016-01-30 14:49:25 -0500

    updated chasers spawn point, lights. Added next button

--- Assets/Script/CoinManager.cs
+++ Assets/Script/CoinManager.cs
@@ -12,8 +12,8 @@ public class CoinManager : MonoBehaviour {
 	void Start () {
 		if (PlayerPrefs.GetInt("Level") <= 8) {
 			for(int i = 0;i < PlayerPrefs.GetInt("Level") / 2; i++){
-				int temp = (mg.ySize/2 - 1) * mg.xSize;
-				int temp2 = (mg.ySize/2 + 1) * mg.xSize;
+				int temp = (mg.ySize/2 - 3) * mg.xSize;
+				int temp2 = (mg.ySize/2 + 3) * mg.xSize;
 				int temp3 = Random.Range (temp, temp2);
 				Instantiate (coin, new Vector2(mg.cells[temp3].x, mg.cells[temp3].y), Quaternion.identity);
 			}
--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -39,15 +39,15 @@ public class LevelManager : MonoBehaviour {
 		case 3:
 		case 4:
 		case 5:
-			Node startPos1 = mg.cells [GenerateChaserPos (true)];
+			Node startPos1 = mg.cells [mg.xSize - 1];//bottom right corner
 			Instantiate (chaser, new Vector2 (startPos1.x, startPos1.y), Quaternion.identity);
 			break;
 		case 6:
 		case 7:
 		case 8:
-			startPos1 = mg.cells [GenerateChaserPos (true)];
+			startPos1 = mg.cells [mg.xSize - 1];//bottom right corner
 			Instantiate (chaser, new Vector2 (startPos1.x, startPos1.y), Quaternion.identity);
-			Node startPos2 = mg.cells[GenerateChaserPos (false)];
+			Node startPos2 = mg.cells[mg.totalCells - mg.xSize];//top left corner
 			Instantiate (chaser, new Vector2(startPos2.x, startPos2.y), Quaternion.identity);
 			break;
 		case 9:
@@ -59,7 +59,7 @@ public class LevelManager : MonoBehaviour {
 		case 12:
 			Camera.main.backgroundColor = Color.black;
 			GameObject.FindGameObjectWithTag ("Player").GetComponentInChildren<Light> ().enabled = true;
-			startPos1 = mg.cells [GenerateChaserPos (true)];
+			startPos1 = mg.cells [mg.xSize - 1];//bottom right corner
 			GameObject tempChaser = Instantiate (chaser, new Vector2 (startPos1.x, startPos1.y), Quaternion.identity) as GameObject;
 			tempChaser.GetComponentInChildren<Light> ().enabled = true;
 			break;
@@ -67,10 +67,10 @@ public class LevelManager : MonoBehaviour {
 		case 14:
 			Camera.main.backgroundColor = Color.black;
 			GameObject.FindGameObjectWithTag ("Player").GetComponentInChildren<Light> ().enabled = true;
-			startPos1 = mg.cells [GenerateChaserPos (true)];
+			startPos1 = mg.cells [mg.xSize - 1];//bottom right corner
 			tempChaser = Instantiate (chaser, new Vector2 (startPos1.x, startPos1.y), Quaternion.identity) as GameObject;
 			tempChaser.GetComponentInChildren<Light> ().enabled = true;
-			startPos2 = mg.cells [GenerateChaserPos (false)];
+			startPos2 = mg.cells [mg.totalCells - mg.xSize];//top left corner
 			GameObject tempChaser2 = Instantiate (chaser, new Vector2 (startPos2.x, startPos2.y), Quaternion.identity) as GameObject;
 			tempChaser2.GetComponentInChildren<Light> ().enabled = true;
 			break;
@@ -136,6 +136,19 @@ public class LevelManager : MonoBehaviour {
 		}
 	}
 
+	public void Next(){
+		if (PlayerPrefs.GetInt ("Level") == 8) {
+			PlayerPrefs.SetInt("xSize", (int)(((double)Screen.width / Screen.height) * 10) - 2);//
+			PlayerPrefs.SetInt("ySize", 10);
+		} 
+		else {
+			PlayerPrefs.SetInt("xSize", PlayerPrefs.GetInt("xSize") + 2 * (Screen.width / Screen.height));
+			PlayerPrefs.SetInt("ySize", PlayerPrefs.GetInt("ySize") + 2);
+		}
+		SceneManager.LoadScene("Maze");
+		PlayerPrefs.SetInt ("Level", PlayerPrefs.GetInt("Level") + 1);
+	}
+
 	void RestartLevel()
 	{
 		// Make sure the texture is enabled.
@@ -160,56 +173,5 @@ public class LevelManager : MonoBehaviour {
 
 	public void Restart(){
 		SceneManager.LoadScene("Maze");
-	}
-
-	int GenerateChaserPos(bool TopOrBottom){//true = spawn the chaser at lower right, false = spawn the chaser at upper left
-		int startCoordinate;
-		int horSize = mg.xSize;
-		int vertSize = mg.ySize;
-
-		int randInt = Random.Range (0, 24);
-
-		if (TopOrBottom) {
-			if (randInt > 0 && randInt < 4) {
-				startCoordinate = horSize - (randInt + 1);
-			} else if (randInt > 5 && randInt < 9) {
-				randInt %= 5;
-				startCoordinate = (2 * horSize) - (randInt + 1);
-			} else if (randInt > 10 && randInt < 14) {
-				randInt %= 5;
-				startCoordinate = (3 * horSize) - (randInt + 1);
-			} else if (randInt > 15 && randInt < 19) {
-				randInt %= 5;
-				startCoordinate = (4 * horSize) - (randInt + 1);
-			} else {
-				randInt %= 5;
-				startCoordinate = (5 * horSize) - (randInt + 1);
-			}
-
-			if (startCoordinate >= horSize * vertSize) 
-				startCoordinate = horSize - 1;
-			
-		}
-		else {
-			if (randInt >= 0 && randInt <= 4) {
-				startCoordinate = (vertSize - 5) * horSize + randInt;
-			} else if (randInt >= 5 && randInt <= 9) {
-				randInt %= 5;
-				startCoordinate = (vertSize - 4) * horSize + randInt;
-			} else if (randInt >= 10 && randInt <= 14) {
-				randInt %= 5;
-				startCoordinate = (vertSize - 3) * horSize + randInt;
-			} else if (randInt >= 15 && randInt <= 19) {
-				randInt %= 5;
-				startCoordinate = (vertSize - 2) * horSize + randInt;
-			} else {
-				randInt %= 5;
-				startCoordinate = (vertSize - 1) * horSize + randInt;
-			}
-
-			if (startCoordinate >= horSize * vertSize) 
-				startCoordinate = horSize - 1;
-		}
-		return startCoordinate;
 	}
 }
